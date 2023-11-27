05ac9970d6bef4d660cb54f37d821ba27851fef3
Cuvuliuc Alex-Andrei <cuvuliucalexandrei@gmail.com>
2023-11-27 13:47:31 +0200

    added readme.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Foobar
+
+timetable-geo-uaic is a PyQt based app where the bachelor students and the university staff from the Faculty of Geography and Geology Iași can view their timetable.
+
+## Installation
+
+To be added.
+
+## Usage
+Run the main.py file.
+
+![how_to_filter_single](./media/filter_single.gif)
+![how_to_filter_multiple](./media/filter_multiple.gif)
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 import io
 from os import path as op
 from setuptools import find_packages
@@ -8,6 +7,9 @@ from setuptools import setup
 version = '0.0.1'
 description = 'A PyQt app where the bachelor students from the University "Alexandru Ioan Cuza" Iași, Faculty of Geography and Geology can view their timetable.'
 here = op.abspath(op.dirname(__file__))
+
+with open('README.md') as f:
+    long_description = f.read()
 
 with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
     core_reqs = f.read().split("\n")
@@ -19,7 +21,7 @@ setup(
     author='Cuvuliuc Alex-Andrei',
     author_email="<cuvuliucalexandrei@gmail.com>",
     description=description,
-    long_description=description,
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/alecsandrei/timetable-geo-uaic',
     packages=find_packages(),
